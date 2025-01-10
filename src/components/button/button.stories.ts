371acc678eb9adff8e707278cfec0bf26c2ab255
strozw <strozw@users.noreply.github.com>
2025-01-10 15:30:37 +0900

    refactor: update button stories render

--- src/components/button/button.stories.ts
+++ src/components/button/button.stories.ts
@@ -1,32 +1,25 @@
 import type { Meta, StoryObj } from "@storybook/html";
-import { parseHtmlToTag } from "../../utils/dom-parser";
 import html from "./button.html?raw";
+import { parseHtml } from "#utils/dom-parser.js";
 
 type ButtonProps = {
-  slot: string;
-  className: string;
+  children: string;
 };
 
 const meta: Meta<ButtonProps> = {
   tags: ["autodocs"],
-  render: ({ slot, className }) => {
-    const button = parseHtmlToTag(html, "button");
+  render: ({ children }) => {
+    const htmlDom = parseHtml(html);
 
-    button.textContent = button.textContent?.replace("{{ $slot }}", slot) ?? "";
+    const button = htmlDom.querySelector("button")!;
 
-    button.className = button.className.replace("{{ $className }}", className);
+    button.textContent = children;
 
-    return button;
+    // script などの付属の dom もレンダリングするため
+    return htmlDom.body.innerHTML;
   },
   args: {
-    slot: "button",
-    className: "",
-  },
-  argTypes: {
-    className: {
-      type: "string",
-      name: "className (※tailwindcss の class はビルドが必要なため反映されない)",
-    },
+    children: "button",
   },
 };
 
